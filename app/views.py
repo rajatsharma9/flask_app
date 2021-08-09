0acf827b40c6a5eaa60d9279442c0fc167cfd8d9
rajatsharma9 <rajsharma@bestpeers.com>
2021-08-09 20:02:11 +0530

    Create crud opration using restful api

--- app/views.py
+++ app/views.py
@@ -1,9 +1,20 @@
 from . import db # noqa
-from .models import Post
-from flask import Blueprint, render_template, redirect, url_for, request
+from .models import Post,User
+from flask import Blueprint, render_template, redirect, url_for, request, jsonify
 from flask_login import current_user
+from flask_restful import Resource, Api, reqparse
+from .schema import users_models_schema, posts_models_schema
+
 
 main = Blueprint('main', __name__)
+api = Api(main)
+
+
+user_post_args = reqparse.RequestParser()
+user_post_args.add_argument('post_title',type=str, help='Title is Required', required=True) 
+user_post_args.add_argument('post_subtitle',type=str, help='SubTitle is Required', required=True)
+user_post_args.add_argument('post_content',type=str, help='Content is Required', required=True)
+user_post_args.add_argument('user_id',type=str, help='User Id is Required', required=True)
 
 
 @main.route('/')
@@ -12,59 +23,54 @@ def home():
     return render_template('home.html')
 
 
-@main.route('/create_post', methods=['GET', 'POST'])
-def create_post():
-    """Here we use Http methods for creating our post by using forms."""
-    if request.method == 'POST':
-        data = {
-            'post_title': request.form['title'],
-            'post_subtitle': request.form['subtitle'],
-            'post_content': request.form['content'],
-        }
-        post_object = Post(data)
-        post_object.user_id = current_user.id
-        db.session.add(post_object)
-        db.session.commit()
-    return render_template('createPost.html')
-
-
-@main.route('/show_all_post/<int:user_id>')
-def user_post(user_id):
-    """Here we get/show the all Post of user by user_id."""
-    user_posts = Post.query.filter(Post.user_id == user_id).order_by(
-        Post.created_date.desc()).all()
-
-    return render_template('showAllPost.html', user_posts=user_posts)
-
-
-@main.route('/update_post/<int:post_id>', methods=['GET', 'POST'])
-def update(post_id):
-    """Here we use Http methods for updating the Post when user clickon update button."""
-    update_post_object = Post.query.filter_by(id=post_id).first()
-    if request.method == 'POST':
-        post_title = request.form['title']
-        post_content = request.form['content']
-        update_post_object.post_title = post_title
-        update_post_object.post_content = post_content
-        db.session.add(update_post_object)
-        db.session.commit()
-        return redirect(
-            url_for('main.user_post', user_id=update_post_object.user_id)
-        )
-
-    return render_template('update.html', user_post=update_post_object)
-
-
-@main.route('/delete_post/<int:post_id>')
-def delete(post_id):
-    """Here we delete the Post when user click on delete button."""
-    user_post_object = Post.query.filter_by(id=post_id).first()
-    db.session.delete(user_post_object)
-    db.session.commit()
-    return redirect(url_for('main.user_post', user_id=user_post_object.user_id))
-
-
 @main.route('/about')
 def about():
     """Show the loged in user post."""
     return render_template('about.html')
+
+
+class PostCrudOprations(Resource):
+    """Here we create API to perform CRUD oprations for user post."""
+
+    def get(self, **kwargs):
+        post_object = Post.query.all()
+        json_post_object = posts_models_schema.dump(post_object)
+        return jsonify(json_post_object)
+
+    def post(self, **kwargs):
+        post_dict = user_post_args.parse_args()
+        data = {
+            'post_title': post_dict['post_title'],
+            'post_subtitle': post_dict['post_subtitle'],
+            'post_content': post_dict['post_content'],
+        }
+        post_object = Post(data)
+        post_object.user_id = post_dict['user_id']
+        db.session.add(post_object)
+        db.session.commit()
+        return "Data Submitted Successful"
+
+    def put(self, post_id):
+        post_dict = user_post_args.parse_args()
+        post_title = post_dict['post_title']
+        post_subtitle = post_dict['post_subtitle']
+        post_content = post_dict['post_content']
+        update_post_object = Post.query.filter_by(id=post_id).first()
+        update_post_object.post_title = post_title
+        update_post_object.post_subtitle = post_subtitle
+        update_post_object.post_content = post_content
+        db.session.add(update_post_object)
+        db.session.commit()
+        return "Data updated Successful"
+
+    def delete(self, post_id):
+        post_object = Post.query.filter_by(id=post_id).first()
+        db.session.delete(post_object)
+        db.session.commit()
+        return "Data delete Successful"
+
+api.add_resource(PostCrudOprations,'/user_post/<int:post_id>')
+
+
+
+
